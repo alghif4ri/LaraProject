e374e091172a4c1a84e4e45aa8041d754a73ae39
LuckyNoobz <r.abudzar@gmail.com>
2024-05-30 15:36:34 +0700

    install bootstrap

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,35 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>LaraProject</title>
+    <link href="{{ asset('bootstrap-5/css/bootstrap.min.css') }}" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="{{ asset('bootstrap-5/js/bootstrap.bundle.min.js') }}"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
     <style>
-        .blog{
+        .blog {
             padding: 5px;
             border-bottom: 1px solid lightgray;
         }
+
         small {
             color: gray;
         }
     </style>
 </head>
+
 <body>
-    <h1>LaraProject</h1>
-    {{-- @foreach ($posts as $post)
-    {{$post}} <br>
-    @endforeach --}}
-    <div>
+    <div class="container">
+        <h1>LaraProject</h1>
         <h2>Post</h2>
         @php($number = 1)
-        @foreach($posts as $post)
         <div class="blog">
-            <h3>{{$post[0]}}<small>#{{$number}}</small></h3> 
-            <p>{{$post[1]}}</p>
+            @foreach ($posts as $post)
+                <h3>{{ $post[0] }}<small>#{{ $number }}</small></h3>
+                <p>{{ $post[1] }}</p>
         </div>
         @php($number++)
         @endforeach
     </div>
 </body>
+
 </html>
