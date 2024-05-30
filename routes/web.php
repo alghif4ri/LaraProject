00c3fb14634c2c4bb786a83ca0ceffd33b5ef8a4
LuckyNoobz <r.abudzar@gmail.com>
2024-05-30 13:13:50 +0700

    array static

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\HelloController;
+// use App\Http\Controllers\HelloController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,6 +20,9 @@ Route::get('/', function () {
 });
 
 // Route::get('hello', 'App\Http\Controllers\HelloController@index');
-Route::post('hello', [HelloController::class, 'create']);
-Route::get('hello', [HelloController::class, 'index']);
-Route::get('world', [HelloController::class, 'world_message']);
+// Route::post('hello', [HelloController::class, 'create']);
+// Route::get('hello', [HelloController::class, 'index']);
+// Route::get('world', [HelloController::class, 'world_message']);
+
+// Route::resource('posts', PostController::class);
+Route::get('posts', [PostController::class, 'index']);
