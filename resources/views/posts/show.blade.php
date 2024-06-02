62c6164f1bde6d96e9fb0b26376a29ee67d15e55
LuckyNoobz <r.abudzar@gmail.com>
2024-06-02 11:13:33 +0700

    show single data

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>LaraProject | {{ $post[1] }}</title>
+    <title>LaraProject | {{ $post->title }}</title>
     <link href="{{ asset('bootstrap-5/css/bootstrap.min.css') }}" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="{{ asset('css/blog.css') }}" rel="stylesheet">
@@ -17,11 +17,13 @@
 <body>
     <div class="container">
         <article class="blog-post">
-            <h2 class="display-5 link-body-emphasis mb-1">{{ $post[1] }}</h2>
-            <p class="blog-post-meta">{{ date('d M Y H:i', strtotime($post[3])) }} <a href="#">Author</a></p>
-            <p>{{ $post[2] }}</p>
+            <h2 class="display-5 link-body-emphasis mb-1">{{ $post->title }}</h2>
+            <p class="blog-post-meta">{{ date('d M Y H:i', strtotime($post->created_at)) }} <a href="#">Author</a>
+            </p>
+            <p>{{ $post->content }}</p>
         </article>
-        <a href="{{ url('posts') }}">< Kembali</a>
+        <a href="{{ url('posts') }}">
+            < Kembali</a>
     </div>
 </body>
 
