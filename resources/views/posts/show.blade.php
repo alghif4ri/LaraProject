3fcdcd96292b858002e4cc228107745bbe4108ca
LuckyNoobz <r.abudzar@gmail.com>
2024-06-13 11:58:44 +0700

    show comments

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -21,6 +21,14 @@
             <p class="blog-post-meta">{{ date('d M Y H:i', strtotime($post->created_at)) }} <a href="#">Author</a>
             </p>
             <p>{{ $post->content }}</p>
+            <small class="text-muted">{{ $total_comments }}</small>
+            @foreach ($comments as $comment)
+                <div class="card mb-3">
+                    <div class="card-body">
+                        <p style="font-size:8pt ">{{ $comment->comment }}</p>
+                    </div>
+                </div>
+            @endforeach
         </article>
         <a href="{{ url('posts') }}">
             < Kembali</a>
