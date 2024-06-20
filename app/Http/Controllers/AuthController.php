0fc9c07b15e38bc1d1a7c7ba68362ee3ee10738a
LuckyNoobz <r.abudzar@gmail.com>
2024-06-20 16:45:31 +0700

    validation input

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -38,7 +38,12 @@ class AuthController extends Controller
         return view('auth.register');
     }
 
-    public function register()
+    public function register(Request $request)
     {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:4|confirmed'
+        ]);
     }
 }
